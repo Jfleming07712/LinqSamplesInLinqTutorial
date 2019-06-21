c321e213f40f391bb047995a07655bc543978ea8
Jfleming <Jfleming07712@gmail.com>
2019-06-20 20:51:15 -0400

    explored basic linq as well as extension methods.

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Features
 {
@@ -6,7 +10,24 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee {ID = 1, Name = "Scott"},
+                new Employee {ID = 2, Name = "Chris"}
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee {ID = 3, Name = "Alex"}
+            };
+
+            Console.WriteLine(developers.Count());
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
         }
     }
 }
