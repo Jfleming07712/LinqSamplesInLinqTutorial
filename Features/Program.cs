e7c9bfe134d0406a97a710028dbb3080e5ec8620
Jfleming <Jfleming07712@gmail.com>
2019-06-20 21:30:33 -0400

    explored the use of func and action.

--- Features/Program.cs
+++ Features/Program.cs
@@ -10,6 +10,13 @@ namespace Features
     {
         static void Main(string[] args)
         {
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) => x + y;
+
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(square((add(3,5))));
+
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {ID = 1, Name = "Scott"},
@@ -21,10 +28,7 @@ namespace Features
                 new Employee {ID = 3, Name = "Alex"}
             };
 
-            foreach (var employee in developers.Where(delegate (Employee employee)
-                {
-                    return employee.Name.StartsWith("S");
-                }))
+            foreach (var employee in developers.Where(e => e.Name.Length ==5).OrderBy(e => e.Name))
             {
                 Console.WriteLine(employee.Name);
 
