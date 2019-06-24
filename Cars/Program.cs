fbb4ce1f2c995c92aa3b872d99d58e12e9ed95cd
Jfleming <Jfleming07712@gmail.com>
2019-06-24 15:59:00 -0400

    Sorted cars by fuel efficiency.

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Cars
 {
@@ -9,11 +11,31 @@ namespace Cars
         {
             var cars = ProcessFile("fuel.csv");
 
+            var query =
+                from car in cars
+                where car.Manufacturer == "BMW" && car.Year == 2016
+                orderby car.Combined descending, car.Name ascending
+                select car;
+
+            var result = cars.Any(c => c.Manufacturer == "Ford");
+
+
+            Console.WriteLine(result);
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Name} : {car.Combined}");
+            }
         }
 
         private static List<Car> ProcessFile(string path)
         {
-            throw new NotImplementedException();
+            var query =
+                from line in File.ReadAllLines(path).Skip(1)
+                where line.Length > 1
+                select Car.ParseFromCsv(line);
+
+            return query.ToList();
         }
     }
 }
