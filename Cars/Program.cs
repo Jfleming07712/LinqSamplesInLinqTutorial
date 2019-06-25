a54b1b2fc4f3bf1599e8c264446414260d578df4
Jfleming <Jfleming07712@gmail.com>
2019-06-25 14:37:58 -0400

    working with aggregation in Linq.  Found the Min, Max, and Avg of the cars fuel efficiency within the manufacturer group.  Then printed those in order of desending value thus putting the most fuel efficient (by max) at the top.

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -15,16 +15,17 @@ namespace Cars
             var manufacturers = ProcessManufacturers("manufacturers.csv");
 
             var query =
-               from manufacturer in manufacturers
-               join car in cars on manufacturer.Name equals car.Manufacturer
-                    into carGroup
-               orderby manufacturer.Name
+               from car in cars
+               group car by car.Manufacturer into carGroup
                select new
                {
-                   Manufacturer = manufacturer,
-                   Cars = carGroup
+                   Name = carGroup.Key,
+                   Max = carGroup.Max(c => c.Combined),
+                   Min = carGroup.Min(c => c.Combined),
+                   Avg = carGroup.Average(c => c.Combined)
                } into result
-               group result by result.Manufacturer.Headquarters;
+               orderby result.Max descending
+               select result;
 
             var query2 =
                 manufacturers.GroupJoin(cars, m => m.Name, c => c.Manufacturer, (m, g) =>
@@ -37,15 +38,12 @@ namespace Cars
 
 
 
-            foreach (var group in query2)
+            foreach (var result in query)
             {
-                Console.WriteLine($"{group.Key}");
-                foreach (var car in group.SelectMany(g => g.Cars)
-                    .OrderByDescending(c=> c.Combined)
-                    .Take(3))
-                {
-                    Console.WriteLine($"\t{car.Name} : {car.Combined}");
-                }
+                Console.WriteLine($"{result.Name}");
+                Console.WriteLine($"\t Max : {result.Max}");
+                Console.WriteLine($"\t Min : {result.Min}");
+                Console.WriteLine($"\t Avg : {result.Avg}");
             }
 
         }
